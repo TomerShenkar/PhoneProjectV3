1fd0673f7bcb39845508f32a3f5950e48c9e2a69
Tomer Shenkar <tomer.shenkar@gmail.com>
2019-02-02 14:56:37 +0200

    WORKING VERSION WITH SMS, PHONE AND CONTACTS

--- PhoneProjV3/src/application/CVController.java
+++ PhoneProjV3/src/application/CVController.java
@@ -13,9 +13,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.ContextMenu;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class CVController extends MainController{
